9b04c736e35b977f2670381e9d77b8033ee31801
nasreddine <90349923+nasreddineelmadhkour@users.noreply.github.com>
2023-11-02 12:06:56 +0100

    JUINT
    test unitaire

--- src/test/java/tn/esprit/devops_project/SupplierServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/SupplierServiceImplTest.java
@@ -1,27 +1,21 @@
 package tn.esprit.devops_project;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.devops_project.entities.Supplier;
-import tn.esprit.devops_project.entities.SupplierCategory;
 import tn.esprit.devops_project.repositories.SupplierRepository;
 import tn.esprit.devops_project.services.SupplierServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
-public class SupplierServiceImplTest {
+class SupplierServiceImplTest {
 
     @Mock
     private SupplierRepository supplierRepository;
@@ -32,71 +26,59 @@ public class SupplierServiceImplTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        supplierService = new SupplierServiceImpl(supplierRepository);
     }
 
     @Test
     void retrieveAllSuppliers() {
         List<Supplier> expectedSuppliers = new ArrayList<>();
-        expectedSuppliers.add(new Supplier().builder().idSupplier(1L).build());
-        expectedSuppliers.add(new Supplier().builder().idSupplier(2L).build());
-        when(supplierRepository.findAll()).thenReturn(expectedSuppliers);
 
+        when(supplierRepository.findAll()).thenReturn(expectedSuppliers);
         List<Supplier> actualSuppliers = supplierService.retrieveAllSuppliers();
 
         assertEquals(expectedSuppliers, actualSuppliers);
+        verify(supplierRepository, times(1)).findAll();
     }
 
     @Test
     void addSupplier() {
-        Supplier expectedSupplier = new Supplier().builder().idSupplier(1L).code("Code").label("Label").supplierCategory(SupplierCategory.ORDINAIRE).build();
-        when(supplierRepository.save(expectedSupplier)).thenReturn(expectedSupplier);
+        Supplier supplier = new Supplier();
 
-        Supplier actualSupplier = supplierService.addSupplier(expectedSupplier);
+        when(supplierRepository.save(supplier)).thenReturn(supplier);
 
-        assertEquals(expectedSupplier, actualSupplier);
+        Supplier addedSupplier = supplierService.addSupplier(supplier);
+
+        assertEquals(supplier, addedSupplier);
+        verify(supplierRepository, times(1)).save(supplier);
     }
 
     @Test
     void updateSupplier() {
-        Supplier expectedSupplier = new Supplier().builder().idSupplier(1L).code("Update Code").label("Update Label").supplierCategory(SupplierCategory.CONVENTIONNE).build();
-        when(supplierRepository.save(expectedSupplier)).thenReturn(expectedSupplier);
+        Supplier supplier = new Supplier();
 
-        Supplier actualSupplier = supplierService.updateSupplier(expectedSupplier);
+        when(supplierRepository.save(supplier)).thenReturn(supplier);
 
-        assertEquals(expectedSupplier, actualSupplier);
+        Supplier updatedSupplier = supplierService.updateSupplier(supplier);
+
+        assertEquals(supplier, updatedSupplier);
+        verify(supplierRepository, times(1)).save(supplier);
     }
 
     @Test
-    void deleteSupplierById() {
+    void deleteSupplier() {
         Long supplierId = 1L;
-
         supplierService.deleteSupplier(supplierId);
-
         verify(supplierRepository, times(1)).deleteById(supplierId);
     }
 
     @Test
-    void retrieveSupplier_ExistingSupplierId() {
+    void retrieveSupplier() {
         Long supplierId = 1L;
         Supplier expectedSupplier = new Supplier();
-        expectedSupplier.setIdSupplier(supplierId);
-        when(supplierRepository.findById(supplierId)).thenReturn(Optional.of(expectedSupplier));
+        when(supplierRepository.findById(supplierId)).thenReturn(java.util.Optional.of(expectedSupplier));
 
         Supplier actualSupplier = supplierService.retrieveSupplier(supplierId);
 
         assertEquals(expectedSupplier, actualSupplier);
-    }
-
-    @Test
-    void retrieveSupplier_NonExistingSupplierId() {
-        Long supplierId = 1L;
-        when(supplierRepository.findById(supplierId)).thenReturn(Optional.empty());
-
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            supplierService.retrieveSupplier(supplierId);
-        });
-
-        assertEquals("Invalid user Id:" + supplierId, exception.getMessage());
+        verify(supplierRepository, times(1)).findById(supplierId);
     }
 }
