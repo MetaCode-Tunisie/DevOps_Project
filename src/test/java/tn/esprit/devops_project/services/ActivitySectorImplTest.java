047a5534b12762f616f7257020c2a13f89d6214b
Sarraelkamel <98896282+Sarraelkamel@users.noreply.github.com>
2023-11-04 16:14:23 +0100

    fixing quality

--- src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
@@ -2,10 +2,12 @@ package tn.esprit.devops_project.services;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -25,7 +27,7 @@ import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ActivitySectorImplTest {
 
     @Autowired
