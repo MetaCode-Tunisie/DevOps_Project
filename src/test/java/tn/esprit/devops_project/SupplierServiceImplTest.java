d48ecad62396d3512e684aff33da2eb0a86db13f
nasreddine <90349923+nasreddineelmadhkour@users.noreply.github.com>
2023-11-06 15:06:05 +0100

    Update SupplierServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/SupplierServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/SupplierServiceImplTest.java
@@ -1,64 +1,84 @@
 package tn.esprit.devops_project;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.devops_project.entities.Supplier;
-import tn.esprit.devops_project.entities.SupplierCategory;
 import tn.esprit.devops_project.repositories.SupplierRepository;
 import tn.esprit.devops_project.services.SupplierServiceImpl;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest
+import static org.mockito.Mockito.*;
 
 class SupplierServiceImplTest {
 
     @Mock
-    SupplierRepository supplierRepository;
+    private SupplierRepository supplierRepository;
 
     @InjectMocks
-    SupplierServiceImpl supplierService;
-    Supplier supplier = new Supplier(1L,"code","label1",SupplierCategory.ORDINAIRE);
-    List<Supplier> supplierList = new ArrayList<Supplier>() {
-        {
-            add(new Supplier(2L,"code2","label2",SupplierCategory.ORDINAIRE));
-            add(new Supplier(3L,"code3","label3",SupplierCategory.CONVENTIONNE));
-        }
-    };
+    private SupplierServiceImpl supplierService;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void retrieveAllSuppliers() {
+        List<Supplier> expectedSuppliers = new ArrayList<>();
+
+        when(supplierRepository.findAll()).thenReturn(expectedSuppliers);
+        List<Supplier> actualSuppliers = supplierService.retrieveAllSuppliers();
+
+        assertEquals(expectedSuppliers, actualSuppliers);
+        verify(supplierRepository, times(1)).findAll();
+    }
 
     @Test
     void addSupplier() {
-        // Create a new supplier object
-        Supplier newSupplier = new Supplier(4L, "code4", "label4", SupplierCategory.ORDINAIRE);
+        Supplier supplier = new Supplier();
 
-        // Set up the mock behavior of the supplierRepository.save() method
-        Mockito.when(supplierRepository.save(Mockito.any(Supplier.class))).thenReturn(newSupplier);
+        when(supplierRepository.save(supplier)).thenReturn(supplier);
 
-        // Call the method under test
-        Supplier savedSupplier = supplierService.addSupplier(newSupplier);
+        Supplier addedSupplier = supplierService.addSupplier(supplier);
 
-        // Verify the interactions and assertions
-        Mockito.verify(supplierRepository, Mockito.times(1)).save(Mockito.any(Supplier.class));
-        assertEquals(newSupplier, savedSupplier);
+        assertEquals(supplier, addedSupplier);
+        verify(supplierRepository, times(1)).save(supplier);
+    }
+
+    @Test
+    void updateSupplier() {
+        Supplier supplier = new Supplier();
+
+        when(supplierRepository.save(supplier)).thenReturn(supplier);
+
+        Supplier updatedSupplier = supplierService.updateSupplier(supplier);
+
+        assertEquals(supplier, updatedSupplier);
+        verify(supplierRepository, times(1)).save(supplier);
+    }
+
+    @Test
+    void deleteSupplier() {
+        Long supplierId = 1L;
+        supplierService.deleteSupplier(supplierId);
+        verify(supplierRepository, times(1)).deleteById(supplierId);
     }
 
     @Test
     void retrieveSupplier() {
-        Mockito.when(supplierRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(supplier));
-        Supplier supplier1 = supplierService.retrieveSupplier(2L);
-        Assertions.assertNotNull(supplier1);
+        Long supplierId = 1L;
+        Supplier expectedSupplier = new Supplier();
+        when(supplierRepository.findById(supplierId)).thenReturn(java.util.Optional.of(expectedSupplier));
+
+        Supplier actualSupplier = supplierService.retrieveSupplier(supplierId);
+
+        assertEquals(expectedSupplier, actualSupplier);
+        verify(supplierRepository, times(1)).findById(supplierId);
     }
-
-
-
 }
